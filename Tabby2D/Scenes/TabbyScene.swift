8b3a42acf61b561f0ba241e397e7393fb295640e
Cyril Wei <teakscn@gmail.com>
2015-12-14 16:56:25 +0800

    add worldlayer

--- Tabby2D/Scenes/TabbyScene.swift
+++ Tabby2D/Scenes/TabbyScene.swift
@@ -8,5 +8,26 @@
 
 import SpriteKit
 
+enum WorldLayer: CGFloat {
+    case StaticBackground = -100, DynamicBackground = -50, Characters = 0, Foreground = 1000, HUD = 1100, Top = 1200
+
+    var nodeName: String {
+        switch self {
+        case .StaticBackground: return "staticbg"
+        case .DynamicBackground: return "dynamicbg"
+        case .Characters: return "characters"
+        case .Foreground: return "foreground"
+        case .HUD: return "HUD"
+        case .Top: return "top"
+        }
+    }
+
+    var nodePath: String {
+        return "/world/\(nodeName)"
+    }
+
+    static var allLayers = [StaticBackground, DynamicBackground, Characters, HUD, Top]
+}
+
 public class TabbyScene: SKScene, TabbyGameInputDelegate {
 }
