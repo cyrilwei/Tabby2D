e4447f87f25bc88a30a4e6ec70aa1b5894f6cc47
Cyril Wei <teakscn@gmail.com>
2016-01-12 08:56:16 +0800

    add TBDirector

--- Tabby2D/TBDirector.swift
+++ Tabby2D/TBDirector.swift
@@ -9,5 +9,34 @@
 import SpriteKit
 
 public class TBDirector {
+    private var stageView: SKView {
+        didSet {
+            initStageView()
+        }
+    }
     
+    private var scenes = [String: TBScene]()
+    
+    public init(stageView: SKView) {
+        self.stageView = stageView
+        initStageView()
+    }
+    
+    public func registerScene(scene: TBScene, withSceneIdentifier sceneID: String) {
+        scenes[sceneID] = scene
+    }
+    
+    public func transitionToSceneWithSceneIdentifier(sceneID: String) {
+        if let scene = scenes[sceneID] {
+            stageView.presentScene(scene)
+        }
+    }
+    
+    private func initStageView() {
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        stageView.ignoresSiblingOrder = true
+        
+        stageView.showsFPS = true
+        stageView.showsNodeCount = true
+    }
 }
